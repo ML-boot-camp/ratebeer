7c90a2f46b07e450296426b1018c54010852a5c0
LouisMlr <lmelliorat@gmail.com>
2022-11-02 21:10:20 +0100

    add script that generate clean sample of raw data

---
+++ cleaned_sample.py
@@ -0,0 +1,60 @@
+import pandas as pd
+
+from utils import (
+    parse, 
+    clean_column_names,
+    clean_integer_features,
+    clean_float_features,
+    clean_categorical_features
+)
+
+def main():
+    """
+    Generate a clean sample of the raw ratebeer json file.
+    The sample is saved as parquet file.
+    """
+
+    data_dir = "data/"
+    raw_filename = "ratebeer.json"    
+
+    # Load the data and select a sample
+    df_raw = (
+        pd.DataFrame.from_records(parse(data_dir+raw_filename))
+        .sample(n=200000, random_state=42)
+    )
+
+    # Define column types
+    integer_features = [
+        "review_appearance",
+        "review_aroma",
+        "review_palate",
+        "review_taste",
+        "review_overall",
+    ]
+
+    float_features = [
+        "beer_ABV",
+    ]
+
+    categorical_features = [
+        "beer_name",
+        "beer_style",
+    ]
+
+    # Apply cleaning steps to the data
+    df_clean = (
+        (df_raw)
+        .pipe(clean_column_names)
+        .pipe(clean_integer_features, integer_features)
+        .pipe(clean_float_features, float_features)
+        .pipe(clean_categorical_features, categorical_features)
+    )
+
+    # Save the cleaned sample of data
+    clean_filename = "ratebeer_sample_clean.parquet"
+    df_clean.to_parquet(data_dir + clean_filename)
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ utils.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import html
+
+def parse(path):
+    with open(path, "r") as f:
+        for line in f:
+            yield eval(line)
+
+
+def clean_column_names(df):
+  df.columns = df.columns.str.replace("/", "_")
+  return df
+
+
+def clean_integer_features(df, columns):
+    df = df.copy()
+    for c in columns:
+        df[c] = df[c].str.split("/").str[0].astype(int)
+    return df
+
+
+def clean_float_features(df, columns):
+    df = df.copy()
+    for c in columns:
+        df[c] = df[c].replace("-", -1).astype(float)
+    return df
+
+
+def clean_categorical_features(df, columns):
+    df = df.copy()
+    for c in columns:
+        df[c] = df[c].apply(html.unescape)
+    return df
